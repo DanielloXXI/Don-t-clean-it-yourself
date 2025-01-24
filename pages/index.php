0068a76b42d0133f20ac42997ccecd645ed142d1
DeadBlazze <pgejmer171@gmail.com>
2025-01-24 15:12:14 +0300

    Написал основную разметку для index.php

---
+++ pages/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Регистрация</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class="col-3 col-md-3 mb-2 mb-md-0">
+                <a href="./index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <img src="../media/logo.webp" alt="Лого" width="60">
+                </a>
+            </div>
+            <ul class="nav mb-2 justify-content-center mb-md-0">
+                <li></li>
+                <li><a href="#" class="btn btn-primary">Sign-up</a></li>
+            </ul>
+        </header>
+        <main class="main">
+            <div class="col-12 col-md-6 mx-auto">
+                <div class="card">
+                    <div class="card-header d-flex">
+                        <button class="btn btn-primary ms-auto">Новая заявка</button>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Заявка от 20.20.24</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    
+                    </div>
+                </div>
+        </main>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="../scripts/script.js"></script>
+</body>
+
+
+</html>
