1476140d086f888168412ed7e68a84ab88368b26
DeadBlazze <pgejmer171@gmail.com>
2025-01-24 14:57:39 +0300

    Добавил валидацию для авторизации

--- pages/auth.php
+++ pages/auth.php
@@ -22,20 +22,53 @@
         <main class="main">
             <div class="col-12 col-md-6 mx-auto">
                 
-                <form action="server.php" method="post" class="border border-1 rounded p-4">
+                <form action="server.php" method="post" class="border border-1 rounded p-4 needs-validation" novalidate>
                     <h2 class="mb-3">Авторизуйтесь</h2>
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <label for="exampleInputEmail1" class="form-label">Login</label>
+                        <input type="text" name="email" class="form-control" id="validationServer01" aria-describedby="emailHelp" value="Otto" minlength="3" required pattern='[0-9A-Za-z\s\-\w]{2,30}'>
+                        <div class="valid-feedback">
+                        Looks good
+                        </div>
+                        <div class="invalid-feedback">
+                        Специальные символы запрещены. Минимум 3 символа
+                        </div>
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" pattern='[0-9A-Za-z\s\-\w]{2,30}' required>
+                        <div class="valid-feedback">
+                        Looks good
+                        </div>
+                        <div class="invalid-feedback">
+                        Минимум 3 символа
+                        </div>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
         </main>
+        <script>
+            (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        forms.forEach(form => {
+            form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+            }, false)
+        })
+        })()
+        </script>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
