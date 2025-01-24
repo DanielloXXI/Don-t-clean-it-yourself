a72dd1a304c5c4b28d03db9234855e5f5f24f202
DeadBlazze <pgejmer171@gmail.com>
2025-01-24 13:45:50 +0300

    Сверстал страницу авторизации

--- pages/auth.php
+++ pages/auth.php
@@ -7,6 +7,35 @@
     <title>Авторизация</title>
 </head>
 <body>
-    
+    <div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class="col-3 col-md-3 mb-2 mb-md-0">
+                <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <img src="../media/logo.webp" alt="" width="60">
+                </a>
+            </div>
+            <ul class="nav mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="btn btn-outline-primary me-2">Login</a></li>
+                <li><a href="#" class="btn btn-primary">Sign-up</a></li>
+            </ul>
+        </header>
+        <main class="main">
+            <div class="col-12 col-md-6 mx-auto">
+                
+                <form action="server.php" method="post" class="border border-1 rounded p-4">
+                    <h2 class="mb-3">Авторизуйтесь</h2>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </main>
+    </div>
 </body>
 </html>
--- pages/reg.php
+++ pages/reg.php
@@ -3,9 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Регистрация</title>
 </head>
 <body>
-    
+    <div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class="col-3 col-md-3 mb-2 mb-md-0">
+                <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <img src="../media/logo.webp" alt="" width="60">
+                </a>
+            </div>
+            <ul class="nav mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="btn btn-outline-primary me-2">Login</a></li>
+                <li><a href="#" class="btn btn-primary">Sign-up</a></li>
+            </ul>
+        </header>
+        <main class="main">
+            <div class="col-12 col-md-6 mx-auto">
+                <form action="server.php" method="post" class="border border-1 rounded p-4">
+                    <h2 class="mb-3">Регистрация</h2>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </main>
+    </div>
 </body>
 </html>
